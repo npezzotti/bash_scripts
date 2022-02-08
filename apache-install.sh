b7d5d89c697c3d7481842110515241eb9983ef9b
npezzotti <nathan.pezzotti@datadoghq.com>
2022-02-08 11:39:50 -0500

    fixed apache script

--- apache-install.sh
+++ apache-install.sh
@@ -4,41 +4,43 @@
 
 if [ $(id -u) != 0 ]
 then
-  echo "* This script requires root privileges"
+  echo "This script requires root privileges"
   exit 1
 fi
 
-echo "* Updating package cache..."
+echo "Updating package cache..."
 apt-get update -qq
-echo "* Installing Apache..."
+echo "Installing Apache..."
 apt-get install -y apache2 > /dev/null
 
-echo "* Allowing Apache app in Firewall..."
+echo "Allowing Apache app in Firewall..."
 ufw allow in "Apache"
 
 UFW_STATUS=$(sudo ufw status | head -n 1 | awk '{print $2}')
 
 if [ $UFW_STATUS == "inactive" ]
 then
-  echo "* WARNING: Firewall inactive..."
+  echo "WARNING: Firewall inactive..."
 fi
 
 TEST_SITE_DIR=/var/www/test
 TEST_SITE_HTML=/var/www/test/index.html
 
+echo "Creating directory /var/www/test..."
+
 if [ -d $TEST_SITE_DIR ]
 then
   echo "/var/www/test already exists, moving on..."
 else
-  echo "* Creating directory /var/www/test..."
   mkdir $TEST_SITE_DIR
 fi
+
+echo "Creating index.html for test site..."
 
 if [ -f $TEST_SITE_HTML ]
 then
   echo "/var/www/test/index.html already exists, moving on..."
 else
-  echo "* Creating test index.html..."
   echo """<html>
   <head>
     <title> Test </title>
@@ -46,24 +48,22 @@ else
   <body>
     <p> This is a test site! </p>
   </body>
-  </html>
-  """ > $TEST_SITE_HTML
+</html>""" > $TEST_SITE_HTML
 
   cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/test.conf
   sed -i 's/DocumentRoot \/var\/www\/html/DocumentRoot \/var\/www\/test/' /etc/apache2/sites-available/test.conf
   sed -i 's/#ServerName www.example.com/ServerName www.test.com/' /etc/apache2/sites-available/test.conf
 fi
 
-echo "* Enabling test site..."
+echo "Enabling test site..."
 a2ensite test
 
-echo "* Reloading Apache..."
+echo "Reloading Apache..."
 systemctl reload apache2
 
 if [  $(systemctl is-active apache2) == "active" ]
 then
-  echo -e "* Installation succesfull! \nRun 'curl localhost -H \"Host: www.test.com\"'"
+  echo -e "Installation succesfull! \nRun 'curl localhost -H \"Host: www.test.com\"'"
 else
-  echo "* Apache failed to start, run systemctl status apache2 to troubleshoot."
+  echo "Apache failed to start, run systemctl status apache2 to troubleshoot."
 fi
-
